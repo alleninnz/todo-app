0a41da5dbaf65caa8ef15aa89a894410d4480ba8
Allen <allenincosmos@gmail.com>
2025-11-09 12:31:58 +1300

    feat: add plugin react compiler

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import storybook from 'eslint-plugin-storybook'
 
 import js from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier'
+import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -23,6 +24,12 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
     },
   },
   ...storybook.configs['flat/recommended'],
--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,15 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwind(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+      },
+    }),
+    tailwind(),
+    tsconfigPaths(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
